b1dfd266ae82eee91410983568073f444ad06c89
Markus D <mar.d@gmx.net>
2023-06-05 14:19:43 +0200

    timeline and uptime analysis

--- dfxlibs/__init__.py
+++ dfxlibs/__init__.py
@@ -17,7 +17,7 @@
    limitations under the License.
 """
 
-__version__ = '0.0.7'
+__version__ = '0.1.0'
 
 from dfxlibs import windows, general, cli
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfxlibs',
-    version='0.0.7',
+    version='0.1.0',
     packages=find_packages(),
     url='https://github.com',
     license='Apache 2.0',
